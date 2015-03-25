4139073a3f41c8e9dfe5001275e9e4e00639021d
germify <germify@users.noreply.github.com>
2015-03-24 21:53:48 -0500

    update

--- server/publications.js
+++ server/publications.js
@@ -1,7 +1,15 @@
-Meteor.publish('questions', function() {
-	return Questions.find();
+Meteor.publish(null, function () {
+    return Meteor.users.find({});
 });
 
-Meteor.publish('userAnswers', function() {
-	return UserAnswers.find();
+Meteor.publish(null, function() {
+    return Meteor.roles.find({});
+});
+
+Meteor.publish("questions", function() {
+	return Questions.find({});
+});
+
+Meteor.publish("userAnswers", function() {
+	return UserAnswers.find({});
 });
